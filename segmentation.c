02f646149d385a7940d6ac6b74ddcb9e650ccbc5
nour <nourbenyoussef73@gmail.com>
2024-10-01 11:04:41 +0200

    Premier exercice

--- segmentation.c
+++ segmentation.c
@@ -5,44 +5,43 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-#define CHAINE "Hello, World!" 
+#define STRING "Hello, World!" 
 
 int global_initialized = 42;
 int global_uninitialized;
 
 int main (){
 
-printf("Adresse des segments mémoire :\n");
+    printf("Memory segment addresses:\n");
 
-printf("Données globales initialisées (Data) : %p\n", &global_initialized);
-printf("Données globales non initialisées (BSS) : %p\n", &global_uninitialized);
+    printf("Initialized global data (Data) : %p\n", &global_initialized);
+    printf("Uninitialized global data (BSS) : %p\n", &global_uninitialized);
 
-printf("Chaîne de caractères (Str) : %p\n", CHAINE);
- 
-int *heap_var = (int *)malloc (sizeof(int));
-printf("Données allouées dynamiquement (Heap) : %p\n", heap_var);
+    printf("String literal (Str) : %p\n", STRING);
+    
+    int *heap_var = (int *)malloc(sizeof(int));
+    printf("Dynamically allocated data (Heap) : %p\n", heap_var);
 
-int stack_variable = 123;
-printf("Données à portée limitée (Stack) : %p\n", &stack_variable);
- 
-printf("Zone mémoire code (Main Function) : %p\n", &main);
+    int stack_variable = 123;
+    printf("Locally scoped data (Stack) : %p\n", &stack_variable);
+    
+    printf("Code memory area (Main Function) : %p\n", &main);
 
-printf("Zone mémoire librairie partagée (LibC Function) : %p\n", &printf);
+    printf("Shared library memory area (LibC Function) : %p\n", &printf);
 
-
-size_t length = 4096; 
-void* mmap_addr = mmap(NULL, length, PROT_READ | PROT_WRITE,
-                           MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
-if (mmap_addr == MAP_FAILED) {
+    size_t length = 4096; 
+    void* mmap_addr = mmap(NULL, length, PROT_READ | PROT_WRITE,
+                            MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    if (mmap_addr == MAP_FAILED) {
         perror("mmap");
-} else {
-        printf("Adresse mmap: %p\n", mmap_addr);
-}
+    } else {
+        printf("mmap address: %p\n", mmap_addr);
+    }
 
-pid_t parent_pid = getpid();
-printf("PID du processus parent : %d\n", parent_pid);
+    pid_t parent_pid = getpid();
+    printf("Parent process PID : %d\n", parent_pid);
 
-pid_t pid = fork(); 
+    pid_t pid = fork(); 
     if (pid == 0) {
         char pid_str[10];
         snprintf(pid_str, sizeof(pid_str), "%d", parent_pid);
@@ -58,10 +57,11 @@ pid_t pid = fork();
         exit(EXIT_FAILURE);
     }
 
-free(heap_var);
-if (mmap_addr != MAP_FAILED) {
-    munmap(mmap_addr, length);
+    free(heap_var);
+    if (mmap_addr != MAP_FAILED) {
+        munmap(mmap_addr, length);
+    }
+
+    return 0;
 }
 
-return 0;
-}
